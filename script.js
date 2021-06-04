48688779e04f2b797750bad252e79eb54c3b7006
KateSerhiienko <kate.serhiienko@gmail.com>
2021-06-04 12:09:47 +0300

    add(structure)

---
+++ script.js
@@ -0,0 +1,25 @@
+const App = {
+  data() {
+    return {
+      placeholderString: "Enter note",
+      inputValue: "",
+      notes: ["ewfef"],
+    }
+  },
+  methods: {
+    inputChange(event) {
+      this.inputValue = event.target.value;
+    },
+    addNote() {
+      if (this.inputValue !== "") {
+        this.notes.push(this.inputValue)
+      };
+      this.inputValue = "";
+    },
+    removeNote(index, $event) {
+      this.notes.splice(index, 1)
+    }
+  }
+}
+
+Vue.createApp(App).mount("#app");
